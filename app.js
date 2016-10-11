560f5fb23bad11a28f5ba4e5561394c647880e38
YOUR FULL NAME <morris.jess26@gmail.com>
2016-10-11 17:24:36 -0600

    fixed scrolling issue

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ var app = angular.module('portfolio', ['ngRoute']);
 app.config(function($routeProvider){
   $routeProvider
   .when('/', {
-    templateUrl: 'partials/home.html',
-    controller: 'HomeController',
-    controllerAs: 'hc'
+    templateUrl: 'partials/home.html'
   })
   .when('/madwire', {
     templateUrl: 'partials/madwire.html'
@@ -16,3 +14,51 @@ app.config(function($routeProvider){
     redirectTo: '/'
   });
 });
+
+
+app.directive('homescroll', [function(){
+  return {
+    templateUrl: 'partials/homecontent.html',
+    link: function(){
+      $('a[href^="#"]').on('click',function (e) {
+          e.preventDefault();
+
+          var target = this.hash;
+          var $target = $(target);
+
+          $('html, body').stop().animate({
+              'scrollTop': $target.offset().top
+          }, 900, 'swing', function () {
+              window.location.hash = target;
+          });
+      });
+    }
+  };
+}]);
+
+
+app.directive('galleryimages', [function(){
+  return {
+    templateUrl: 'partials/gallery.html',
+    link: function(){
+
+      	$('.gallery').magnificPopup({
+      		delegate: 'a',
+      		type: 'image',
+      		tLoading: 'Loading image #%curr%...',
+      		mainClass: 'mfp-img-mobile',
+      		gallery: {
+      			enabled: true,
+      			navigateByImgClick: true,
+      			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      		},
+      		image: {
+      			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      			titleSrc: function(item) {
+      				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+      			}
+      		}
+      	});
+    }
+  };
+}]);
